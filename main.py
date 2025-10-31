d6c2fe91ac62937d2d58c8d5a02f139ce6879587
Tri Tran <chemandtech874@gmail.com>
2025-10-30 21:33:01 -0400

    store vectors in json file

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-def create_vectors(filename: str) -> dict:
-    families = dict() # Holds the aggregated feature vectors for all malware families
+import json
+
+def create_vectors(filename: str) -> json:
+    # Holds the aggregated feature vectors for all malware families
+    families = dict()
     label_file = open(filename, 'r')
 
     for line in label_file:
         line = line.strip()
         tokens = line.split(",")
 
-        if (tokens[0] != "sha256"): # Skip the header line
+        # Skip the header line
+        if (tokens[0] != "sha256"):
             families[tokens[1]] = dict()
 
+            # Open the file corresponding to the hash
             vector_file = open("feature_vectors/" + tokens[0], 'r')
             for feature in vector_file:
                 feature = feature.strip()
@@ -19,8 +24,16 @@ def create_vectors(filename: str) -> dict:
 
     label_file.close()
 
+    families_json = json.dumps(families, indent=4)
+    families_file = open("families.json", "w")
+    families_file.write(families_json)
+    families_file.close()
+
+    return families_json
+
+
 def main():
-    create_vectors("sha256_family.csv")
+    vectors = create_vectors("sha256_family.csv")
 
 if __name__ == "__main__":
     main()
